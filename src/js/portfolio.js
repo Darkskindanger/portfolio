76493d95d28b59fcba17678c558a68826b0e47b0
Darkskindanger <117239789+Darkskindanger@users.noreply.github.com>
2024-05-13 07:22:21 +1000

    Add files via upload

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,15 +1,22 @@
+let sidebar = document.querySelector("#sideBar");
+let sideBarLinks = document.querySelector("#nav-links");
+let sideBarFooter = document.querySelector("#nav-footer");
+let art1Btn = document.querySelector("#art1-btn");
+
 // Open and close sidebar
 let open = document.querySelector("#menuBtn").addEventListener("click", () => {
-  document.querySelector("#sideBar").style.display = "inline-block";
-  if ((document.querySelector("#sideBar").style.display = "inline-block")) {
-    document.querySelector("#menuBtn").style.display = "none";
+  if ((sidebar.style.width = "0px")) {
+    sidebar.style.width = "320px";
+    sideBarLinks.style.display = "inline-flex";
+    sideBarFooter.style.display = "inline-block";
+    art1Btn.style.display = "none";
   }
 });
 let close = document
   .querySelector("#menuClose")
   .addEventListener("click", () => {
-    document.querySelector("#sideBar").style.display = "none";
-    if ((document.querySelector("#sideBar").style.display = "none")) {
-      document.querySelector("#menuBtn").style.display = "inline-block";
-    }
+    document.querySelector("#sideBar").style.width = "0px";
+    sideBarLinks.style.display = "none";
+    sideBarFooter.style.display = "none";
+    art1Btn.style.display = "inline-block";
   });
